1d89e66ea1e904aa763f4e29d392bd3f8ed676ae
Sonja Peschke <sonjape91@gmail.com>
2022-08-24 15:10:35 +0200

    Added 5 restaurant records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+restaurants = Restaurant.create([
+                                  { name: "Burgermeister", address: "berlin", category: "belgian" },
+                                  { name: "Malafemmena", address: "berlin", category: "italian" },
+                                  { name: "YaMe NumNums", address: "berlin", category: "japanese" },
+                                  { name: "Le Soufflé", address: "paris", category: "french" },
+                                  { name: "Boulangerie Utopie", address: "paris", category: "french" }
+  ])
